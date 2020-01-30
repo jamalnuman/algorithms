c0b604478b9a09c19898d3f50de60c0a7a46b31a
“jamalnuman” <jnuman82@gmail.com>
2020-01-30 16:47:02 -0600

    remove_duplicates in place

---
+++ algorithms.rb
@@ -0,0 +1,14 @@
+# def remove_duplicates(nums)
+#     index = 0
+#     while index < nums.length
+#       if nums[index] == nums[index+1]
+#         nums.delete_at(index+1)
+#         next
+#       end
+#       index += 1
+#     end
+#     return nums.length
+# end
+
+# p remove_duplicates([0,0,1,1,1,2,2,3,3,4])
+
